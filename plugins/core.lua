6f023458644423a05ef09a5971766c5b4d0bdc38
Jobin Nelson <jobinnelson369@gmail.com>
2023-03-30 23:59:33 +0530

    add filename to the statusline
    Default is to show only filetype which is not informative

--- plugins/core.lua
+++ plugins/core.lua
@@ -81,6 +81,29 @@ return {
       return opts
     end,
   },
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require("astronvim.utils.status")
+      opts.statusline = {
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode(),
+        status.component.git_branch(),
+        status.component.file_info { filename = { fallback = "Empty" }, file_modified = false },
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.treesitter(),
+        status.component.nav(),
+        status.component.mode { surround = { separator = "right" } },
+      }
+
+      return opts
+    end,
+  },
   -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = false },
   --
