2253f059d49be817914ae49cf181b9a8435fa4e3
Jobin Nelson <jobinnelson369@gmail.com>
2023-04-21 16:19:44 +0530

    add mapping for neo tree toggle to open file picker
    by default neotree opens buffer picker if you don't have any files
    opened

--- mappings.lua
+++ mappings.lua
@@ -9,6 +9,7 @@ return {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>e"] = { "<cmd>Neotree source=filesystem toggle=true<cr>", desc = "Toggle tree" },
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
