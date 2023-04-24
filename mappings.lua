12c9880128beda3def66df85f740f4b292856e9c
Jobin Nelson <jobinnelson369@gmail.com>
2023-04-24 20:58:47 +0530

    remove keybinding <leader>o in neotree for orgmode

--- mappings.lua
+++ mappings.lua
@@ -8,6 +8,7 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    ["<leader>o"] = false,
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>e"] = { "<cmd>Neotree source=filesystem toggle=true<cr>", desc = "Toggle tree" },
     ["<leader>bD"] = {
