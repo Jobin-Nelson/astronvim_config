f9d437703e0038cf243cc567a537b4d091d3e525
Jobin Nelson <jobinnelson369@gmail.com>
2023-04-28 18:26:21 +0530

    add mapping for opening org refiles

--- plugins/user.lua
+++ plugins/user.lua
@@ -35,13 +35,17 @@ return {
     config = function()
       require('orgmode').setup_ts_grammar()
       require('orgmode').setup({
-        org_agenda_files = { '~/playground/projects/org_files/**/*' },
+        org_agenda_files = {
+          '~/playground/projects/org_files/**/*',
+          '~/playground/dev/illumina/ticket_notes/work/**/*',
+        },
         org_default_notes_file = '~/playground/projects/org_files/refile.org',
-        org_todo_keywords = { 'TODO(t)', 'NEXT(n)', '|', 'DONE(d)' },
+        org_todo_keywords = { 'TODO(t)', 'NEXT(n)', '|', 'DONE(d)', 'CANCELLED(c)' },
         org_archive_location = '~/playground/projects/org_files/archive_file.org',
         org_hide_leading_stars = false,
         org_hide_emphasis_markers = true,
         org_agenda_span = 'day',
+        org_blank_before_new_entry = { heading = false, plain_list_item = false },
       })
     end
   }
