502862bbe39528804a966ea090a4a1966fafe496
Jobin Nelson <jobinnelson369@gmail.com>
2023-11-04 19:23:25 +0530

    add gen.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -64,5 +64,9 @@ return {
     'nvim-treesitter/playground',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     cmd = 'TSPlaygroundToggle',
+  },
+  {
+    'David-Kunz/gen.nvim',
+    event = 'VeryLazy',
   }
 }
